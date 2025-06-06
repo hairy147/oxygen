845dc998d9f2bd21d9e142a1f3b44aaf6f03bdc9
hairy147 <48612872+hairy147@users.noreply.github.com>
2025-06-06 18:32:05 +0100

    Enable multisite configuration

--- wp-config.php
+++ wp-config.php
@@ -92,6 +92,13 @@ define( 'WP_DEBUG', false );
 
 
 define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/oxygen/';
+define( 'DOMAIN_CURRENT_SITE', 'localhost' );
+define( 'PATH_CURRENT_SITE', '/oxygen/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
